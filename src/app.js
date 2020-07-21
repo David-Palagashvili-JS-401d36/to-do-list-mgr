c97fec9c78f9a60d3771fb7e91946c352ddbf964
Davidoffili <palagashvilid@gmail.com>
2020-07-21 12:01:56 -0700

    added linter and gitignore files, installed dependencies

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import { Nav, Navbar}  from 'react-bootstrap';
+
 import ToDo from './components/todo/todo.js';
 
-export default class App extends React.Component {
-  render() {
+export default class App extends React.Component {}
+
+// React compenents/functions should return JSX.
+export default () => {
+//   render() {
     return (
       <>
         <ToDo />
       </>
     );
-  }
 }
